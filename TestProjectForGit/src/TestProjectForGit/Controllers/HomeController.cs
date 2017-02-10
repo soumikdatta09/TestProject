1682210c2d356654534fbc2103bcad7ff75a5879
595282 <595282@PC355686>
2017-02-10 16:18:30 +0530

    change test

--- TestProjectForGit/src/TestProjectForGit/Controllers/HomeController.cs
+++ TestProjectForGit/src/TestProjectForGit/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace TestProjectForGit.Controllers
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
+            ViewData["Message"] = "Your contact page here.";
 
             return View();
         }
